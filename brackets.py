008fb1ecadb9ba7b86dba9cac1b5b1c94f79c7dd
Olesia2805 <vysotina2002@gmail.com>
2024-07-15 10:08:28 +0200

    print format

--- brackets.py
+++ brackets.py
@@ -16,13 +16,14 @@ def find_correct_position_brackets (str):
             
     return 'valid'
 
-print ('1: ' + find_correct_position_brackets('(())')) #valid
-print ('2: ' + find_correct_position_brackets('')) #valid
-print ('3: ' + find_correct_position_brackets('(()')) #invalid
-print ('4: ' + find_correct_position_brackets(')(()')) #invalid
-print ('5: ' + find_correct_position_brackets('(((')) #invalid
+print (f'1 - (()): {find_correct_position_brackets('(())')}') #valid
+print (f'2 - "": {find_correct_position_brackets('')}') #valid
+print (f'3 - ((): {find_correct_position_brackets('(()')}') # invalid
+print (f'4 - )((): {find_correct_position_brackets(')(()')}') #invalid
+print (f'5 - (((: {find_correct_position_brackets('(((')}') #invalid
+print (f'6 - ))): {find_correct_position_brackets(')))')}') #invalid
 
-print ('6: ' + find_correct_position_brackets('({)}')) #invalid
-print ('7: ' + find_correct_position_brackets('[{()}]')) #valid
-print ('8: ' + find_correct_position_brackets('{[(])}')) #invalid
-print ('9: ' + find_correct_position_brackets('({]}')) #invalid
+print ('7 - [{()}]: ' + find_correct_position_brackets('[{()}]')) #valid
+print ('8 - {[(])}: ' + find_correct_position_brackets('{[(])}')) #invalid
+print ('9 - ({]}: ' + find_correct_position_brackets('({]}')) #invalid
+print ('10 - ({)}: ' + find_correct_position_brackets('({)}')) #invalid
